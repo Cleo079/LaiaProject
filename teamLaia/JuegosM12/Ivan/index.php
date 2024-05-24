6149a262d37d451e81aa8d9f1b3bd72dfe0e641b
Cleo079 <126862358+Cleo079@users.noreply.github.com>
2024-05-24 23:18:07 +0200

    Add files via upload

---
+++ teamLaia/JuegosM12/Ivan/index.php
@@ -0,0 +1,71 @@
+<?php
+    require_once('../../landingPageM12/landing/php_libraries/db.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://cdn.discordapp.com/attachments/821868428985630772/1180203519895158875/Eevee_inicial_icono.png" 
+    alt="Isaac Chino" type="image/vnd.microsoft.icon">
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.0/dist/confetti.browser.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
+    <section class="counter-container">
+        <span class="counter">240</span>
+        <span class="pista" onmouseover="mostrarPrevisualizacion(this)" onmouseout="ocultarPrevisualizacion()">
+            <p>?</p>
+        </span>
+        <img id="previsualizacion" class="previsualizacion" src="./Imagenes/Lion Image.png" alt="Lion Image" 
+        width="100px" height="100px">
+    </section>
+
+    <div class="board"></div>
+
+    <section class="first-screen">
+        <h2>Completa el puzzle</h2>
+        <div class="textContainer">
+            <p class="textoLoreKenya">Laia viajó a Kenia, durante</br>
+                su visita vió muchos animales,</br>
+                que de entre ellos uno destacó</br>
+                más que el resto.</br>
+                Deberas hacer click en la pieza</br>
+                para moverla y completar el puzzle.
+            </p>
+        </div>
+    </section>
+
+    <div class="contenedorBotones">
+        <section class="backBtn-container">
+            <a href="../../landingPageM12/gameInterface/carrouselJuegos.html"><button class="back-btn" id="back">BACK</button></a>
+            <div class="back-btn-shadow"></div>
+        </section>
+        
+        <section class="startBtn-container">
+            <button class="play-btn" id="start">START</button>
+            <div class="btn-shadow"></div>
+        </section>
+
+        <!-- añadido para pasar al siguinte juego y que se guarde en la base de datos por php -->
+        <div class="restartButton">
+            <form class="scoreForm" action="../../landingPageM12/landing/php_controllers/userController.php" method="POST">
+                <input name="id_user" type="hidden" value="<?php echo $_SESSION['user']?> ">
+                <input id="scoreInput" name="scoreDataCollect" type="hidden" value="">
+                <button type="submit" class="nextGameButton" name="nextGameButtonKenya">Siguiente juego</button>
+            </form>    
+            <!-- Restart -->
+        </div>
+    </div>
+
+
+
+    <script src="./script.js"></script>
+</body>
+</html>
